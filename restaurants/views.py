b6b7c6fbcbd7178c78f6b7acc6615fbd2719ac0d
Sa7ar Nollily <sa7arnollily@Sahar-2.local>
2019-07-23 13:55:06 +0300

    sahar

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    context = {
+        "msg": "Hello World!",
+    }
+
+    return render(request,'file.html',context)
